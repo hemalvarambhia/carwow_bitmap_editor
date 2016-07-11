40fd3298829a25fa3ee2d84292ed88858f089239
Hemal <h.n.varambhia@gmail.com>
2016-07-11 11:56:03 +0100

    We print the image that was actually generated to give a more helpful failure message

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -50,8 +50,10 @@ describe 'Editing a Bitmap' do
       end
 
       failure_message do |actual|
+        actual_image = actual.map {|row| row.join ' '}.join("\n")
         message = "Expected a #{width} x #{height} white image but got:\n"
-        message << "#{actual.sample.size} x #{actual.size} white image"
+        message << "#{actual.sample.size} x #{actual.size} white image:\n"
+        message << actual_image
         message
       end
     end
