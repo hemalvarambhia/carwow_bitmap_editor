88bd828ed022f5f92c6fe42408e86ed9cf35018b
Hemal <h.n.varambhia@gmail.com>
2016-07-12 13:09:45 +0100

    Moved command interpretation code to helper methods - Command pattern appears to be emerging

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -12,17 +12,9 @@ class BitmapEditor
   def run
     command = @input.gets.strip
     type, args = parse command
-    @bitmap_image = BitmapImage.new(
-       width: args.first.to_i, height: args.last.to_i) if type == 'I'
-
-    col_index = args.first.to_i - 1
-    row_index = args[1].to_i - 1
-    color = args.last
-    @bitmap_image.assign_colour(
-      row: row_index, column: col_index, colour: color) if type =='L'
-
-    @output.puts @bitmap_image.to_s if type == 'S'
-
+    initialise_image args if type == 'I'
+    assign_colour args if type == 'L'
+    show_contents if type == 'S'
     exit if type == 'X'
   end
 
@@ -32,6 +24,23 @@ class BitmapEditor
     type, *args = command.split ' '
 
     return type, args
+  end
+
+  def initialise_image args
+    @bitmap_image = BitmapImage.new(
+       width: args.first.to_i, height: args.last.to_i)
+  end
+
+  def assign_colour args
+    col_index = args.first.to_i - 1
+    row_index = args[1].to_i - 1
+    color = args.last
+    @bitmap_image.assign_colour(
+      row: row_index, column: col_index, colour: color)
+  end
+
+  def show_contents
+    @output.puts @bitmap_image.to_s
   end
 
   class BitmapImage
