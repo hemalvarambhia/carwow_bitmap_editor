ca41d1dacb92ea0b21dd3850041b3bbcb5852d2e
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-07-13 19:49:37 +0100

    Moved duplicate arrange code to a setup method

--- spec/painting_a_horizontal_line_spec.rb
+++ spec/painting_a_horizontal_line_spec.rb
@@ -1,35 +1,35 @@
 require 'bitmap_editor'
 describe 'Bitmap Editor' do
   describe "drawing a horizonal line" do
+    before :each do
+      @input = double(:input, print: nil)
+      @editor = BitmapEditor.new(@input, nil)
+    end
+    
     it 'paints only the horizontal line specified' do
-      input = double(:input, print: nil)
-      allow(input).to receive(:gets).and_return 'I 5 3', 'H 1 5 2 W'
-      editor = BitmapEditor.new(input, nil)
-      3.times { editor.run }
+      allow(@input).to receive(:gets).and_return 'I 5 3', 'H 1 5 2 W'
+      3.times { @editor.run }
 
-      image = editor.image
+      image = @editor.image
       horizontal_segment = image[1][0..-1]
       expect(horizontal_segment).to eq ['W', 'W', 'W', 'W', 'W']
     end
 
     it 'paints a horizontal line anywhere' do
-      input = double(:input, print: nil)
-      allow(input).to receive(:gets).and_return 'I 5 3', 'H 1 2 3 W'
-      editor = BitmapEditor.new(input, nil)
-      3.times { editor.run }
+      allow(@input).to receive(:gets).and_return 'I 5 3', 'H 1 2 3 W'
+      3.times { @editor.run }
 
-      image = editor.image
+      image = @editor.image
       horizontal_segment = image[2][0..1]
       expect(horizontal_segment).to eq ['W', 'W']
     end
 
     it 'paints a horizontal line anywhere any colour' do
-      input = double(:input, print: nil)
-      allow(input).to receive(:gets).and_return 'I 5 3', 'H 1 2 3 Z'
-      editor = BitmapEditor.new(input, nil)
-      3.times { editor.run }
+      allow(@input).to receive(:gets).and_return 'I 5 3', 'H 1 2 3 Z'
 
-      image = editor.image
+      3.times { @editor.run }
+
+      image = @editor.image
       horizontal_segment = image[2][0..1]
       expect(horizontal_segment).to eq ['Z', 'Z']
     end
