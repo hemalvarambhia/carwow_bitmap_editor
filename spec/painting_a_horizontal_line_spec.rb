53296fd1d54add53cb012ff236b67e133187744a
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-07-13 19:58:13 +0100

    Replaced the canvas with a test double and confirmed that the editor collaborates with it correctly

--- spec/painting_a_horizontal_line_spec.rb
+++ spec/painting_a_horizontal_line_spec.rb
@@ -3,35 +3,38 @@ describe 'Bitmap Editor' do
   describe "drawing a horizonal line" do
     before :each do
       @input = double(:input, print: nil)
-      @editor = BitmapEditor.new(@input, nil)
+      @canvas = double(:canvas)
+      @editor = BitmapEditor.new(@input, nil, @canvas)
     end
     
     it 'paints only the horizontal line specified' do
-      allow(@input).to receive(:gets).and_return 'I 5 3', 'H 1 5 2 W'
-      3.times { @editor.run }
+      allow(@input).to receive(:gets).and_return 'H 1 5 2 W'
+      (0..4).each do |column|
+        expect(@canvas).to(
+          receive(:paint).with(row: 1, column: column, colour: 'W'))
+      end
 
-      image = @editor.image
-      horizontal_segment = image[1][0..-1]
-      expect(horizontal_segment).to eq ['W', 'W', 'W', 'W', 'W']
+      @editor.run
     end
 
     it 'paints a horizontal line anywhere' do
-      allow(@input).to receive(:gets).and_return 'I 5 3', 'H 1 2 3 W'
-      3.times { @editor.run }
+      allow(@input).to receive(:gets).and_return 'H 1 2 3 W'
+      (0..1).each do |column|
+        expect(@canvas).to(
+          receive(:paint).with(row: 2, column: column, colour: 'W'))
+      end
 
-      image = @editor.image
-      horizontal_segment = image[2][0..1]
-      expect(horizontal_segment).to eq ['W', 'W']
+      @editor.run
     end
 
     it 'paints a horizontal line anywhere any colour' do
-      allow(@input).to receive(:gets).and_return 'I 5 3', 'H 1 2 3 Z'
+      allow(@input).to receive(:gets).and_return 'H 1 2 3 Z'
+      (0..1).each do |column|
+        expect(@canvas).to(
+          receive(:paint).with(row: 2, column: column, colour: 'Z'))
+      end
 
-      3.times { @editor.run }
-
-      image = @editor.image
-      horizontal_segment = image[2][0..1]
-      expect(horizontal_segment).to eq ['Z', 'Z']
+      @editor.run
     end
   end
 end
