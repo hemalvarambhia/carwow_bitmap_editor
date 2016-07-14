d52e87166284559850be0377ef771001532067a8
Hemal <h.n.varambhia@gmail.com>
2016-07-14 11:17:05 +0100

    Moved duplicate arrange code to setup methods

--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -1,16 +1,18 @@
 require 'bitmap_editor'
 describe 'A Canvas' do
+  before :each do
+    @canvas = BitmapEditor::Canvas.new
+  end
+
   describe '#blank' do
     it 'creates a blank canvas with the specified dimensions' do
-      canvas = BitmapEditor::Canvas.new
-      image = canvas.blank(width: 3 ,height: 3)
+      image = @canvas.blank(width: 3 ,height: 3)
 
       expect(image).to be_width(3).and be_height(3).and be_white
     end
 
     it 'creates a blank canvas of any specified dimensions' do
-      canvas = BitmapEditor::Canvas.new
-      image = canvas.blank(width: 5 ,height: 4)
+      image = @canvas.blank(width: 5 ,height: 4)
 
       expect(image).to be_width(5).and be_height(4).and be_white
     end
@@ -37,39 +39,33 @@ describe 'A Canvas' do
 
   describe '#to_s' do
     it 'renders the pixels as a string' do
-      canvas = BitmapEditor::Canvas.new
-      canvas.blank(width: 4, height: 2)
+      @canvas.blank(width: 4, height: 2)
 
-      expect(canvas.to_s).to eq "OOOO\nOOOO"
+      expect(@canvas.to_s).to eq "OOOO\nOOOO"
     end
   end
 
   describe '#paint' do
+    before :each do
+      @canvas.blank(width: 4, height: 3)
+    end
+
     it 'paints the given pixel a colour' do
-      canvas = BitmapEditor::Canvas.new
-      canvas.blank(width: 4, height: 3)
+      @canvas.paint(row: 0, column: 1, colour: 'H')
 
-      canvas.paint(row: 0, column: 1, colour: 'H')
-
-      expect(canvas.image[0][1]).to eq 'H'
+      expect(@canvas.image[0][1]).to eq 'H'
     end
 
     it 'paints any pixel a colour' do
-      canvas = BitmapEditor::Canvas.new
-      canvas.blank(width: 4, height: 3)      
-
-      canvas.paint(row: 1, column: 2, colour: 'H')  
+      @canvas.paint(row: 1, column: 2, colour: 'H')  
    
-      expect(canvas.image[1][2]).to eq 'H'
+      expect(@canvas.image[1][2]).to eq 'H'
     end
 
     it 'paints any pixel any colour' do
-      canvas = BitmapEditor::Canvas.new
-      canvas.blank(width: 4, height: 3)
+      @canvas.paint(row: 2, column: 2, colour: 'X')       
 
-      canvas.paint(row: 2, column: 2, colour: 'X')       
-
-      expect(canvas.image[2][2]).to eq 'X'
+      expect(@canvas.image[2][2]).to eq 'X'
     end
   end
 end
