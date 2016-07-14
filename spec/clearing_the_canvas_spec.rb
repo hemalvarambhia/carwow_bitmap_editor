de8e8ec942996b84fe00aac6373ace85f01eded1
Hemal <h.n.varambhia@gmail.com>
2016-07-14 14:10:13 +0100

    Test-drove the clear canvas command

--- spec/clearing_the_canvas_spec.rb
+++ spec/clearing_the_canvas_spec.rb
@@ -1,29 +1,26 @@
 require 'bitmap_editor'
 
 describe 'Bitmap Editor' do
-  describe 'clearing the canvas' do
-    before :each do
-      @input = double(:input, print: nil)
-      @canvas = double(:canvas).as_null_object
+  class ClearCanvas
+    def initialize canvas
+      @canvas = canvas
     end
 
-    context 'when there is no image' do
-      it 'does nothing' do
-        allow(@input).to receive(:gets).and_return 'C'
-        expect(@canvas).to receive :clear
-        editor = BitmapEditor.new(@input, nil, @canvas)
-        
-        editor.run
-      end
+    def run args
+      @canvas.clear
+    end
+  end
+
+  describe 'clearing the canvas' do
+    before :each do
+      @canvas = double(:canvas).as_null_object
+      @clear_canvas = ClearCanvas.new @canvas
     end
 
     it 'wipes the canvas clean' do
-      allow(@input).to receive(:gets).and_return 'C'
       expect(@canvas).to receive :clear
 
-      editor = BitmapEditor.new(@input, nil, @canvas)
-
-      editor.run
+      @clear_canvas.run []
     end
   end
 end
