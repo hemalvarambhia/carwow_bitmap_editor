00b8a0d0d14f7ba1b0045a239bad46998ef34b7f
Hemal <h.n.varambhia@gmail.com>
2016-07-14 14:42:03 +0100

    Test-drove the help command

--- spec/getting_help_spec.rb
+++ spec/getting_help_spec.rb
@@ -2,13 +2,11 @@ require 'bitmap_editor'
 describe 'Bitmap Editor' do
   describe 'getting help' do
     it 'displays advice on how to use the commands' do
-      input = double(:input, print: nil)
-      allow(input).to receive(:gets).and_return '?'
       io_output = double :output
       expect(io_output).to receive(:puts).with BitmapEditor::HELP
-      editor = BitmapEditor.new(input, io_output, nil)
+      help = BitmapEditor::Help.new(io_output)
 
-      editor.run
+      help.run []
     end
   end
 end
