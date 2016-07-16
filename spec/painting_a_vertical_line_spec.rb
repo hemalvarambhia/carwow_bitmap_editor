1e16d404dcb18bc1b155af93525edf46e1d2ec03
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-07-16 21:27:17 +0100

    These examples describe correct usage

--- spec/painting_a_vertical_line_spec.rb
+++ spec/painting_a_vertical_line_spec.rb
@@ -5,50 +5,52 @@ describe 'Painting a vertical line on the canvas' do
     @draw_vertical_line = Commands::PaintVerticalLine.new(@canvas)
   end
 
-  it 'paints a vertical line on the specified part' do
-    (3..6).each do |row|
-      expect(@canvas).to(
-          receive(:paint).with(column: 2, row: row, colour: 'W'))
-    end
-
-    @draw_vertical_line.run [2, 3, 6, 'W']
-  end
-
-  it 'paints a vertical line anywhere' do
-    (1..3).each do |row|
-      expect(@canvas).to(
-          receive(:paint).with(column: 5, row: row, colour: 'W'))
-    end
-
-    @draw_vertical_line.run [5, 1, 3, 'W']
-  end
-
-  it 'paints a vertical line anywhere in any colour' do
-    (2..4).each do |row|
-      expect(@canvas).to(
-          receive(:paint).with(column: 4, row: row, colour: 'H'))
-    end
-
-    @draw_vertical_line.run [4, 2, 4, 'H']
-  end
-
-  it 'paints a vertical line in an upward direction' do
-    (2..4).each do |row|
-      expect(@canvas).to(
-          receive(:paint).with(column: 4, row: row, colour: 'H'))
-    end
-
-    @draw_vertical_line.run [4, 4, 2, 'H']
-  end
-
-  context 'when it receives extra parameters' do
-    it 'ignores them' do
-      (2..3).each do |row|
+  describe 'Correct usage' do
+    it 'paints a vertical line on the specified part' do
+      (3..6).each do |row|
         expect(@canvas).to(
-            receive(:paint).with(column: 1, row: row, colour: 'H'))
+          receive(:paint).with(column: 2, row: row, colour: 'W'))
       end
 
-      @draw_vertical_line.run [1, 2, 3, 'H', 'N']
+      @draw_vertical_line.run [2, 3, 6, 'W']
+    end
+
+    it 'paints a vertical line anywhere' do
+      (1..3).each do |row|
+        expect(@canvas).to(
+          receive(:paint).with(column: 5, row: row, colour: 'W'))
+      end
+
+      @draw_vertical_line.run [5, 1, 3, 'W']
+    end
+
+    it 'paints a vertical line anywhere in any colour' do
+      (2..4).each do |row|
+        expect(@canvas).to(
+          receive(:paint).with(column: 4, row: row, colour: 'H'))
+      end
+
+      @draw_vertical_line.run [4, 2, 4, 'H']
+    end
+
+    it 'paints a vertical line in an upward direction' do
+      (2..4).each do |row|
+        expect(@canvas).to(
+          receive(:paint).with(column: 4, row: row, colour: 'H'))
+      end
+
+      @draw_vertical_line.run [4, 4, 2, 'H']
+    end
+
+    context 'when it receives extra parameters' do
+      it 'ignores them' do
+        (2..3).each do |row|
+          expect(@canvas).to(
+            receive(:paint).with(column: 1, row: row, colour: 'H'))
+        end
+
+        @draw_vertical_line.run [1, 2, 3, 'H', 'N']
+      end
     end
   end
 end
