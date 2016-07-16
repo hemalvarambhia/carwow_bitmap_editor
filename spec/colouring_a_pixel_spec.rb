8f1562c7b8d875ad63a5549311a37cc40ea5420d
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-07-16 16:02:36 +0100

    These are examples of correct usage

--- spec/colouring_a_pixel_spec.rb
+++ spec/colouring_a_pixel_spec.rb
@@ -5,29 +5,32 @@ describe 'Colouring a pixel on the canvas' do
     @colour_pixel = Commands::ColourPixel.new(@canvas)
   end
 
-  it 'colours in the specified pixel' do
-    expect(@canvas).to receive(:paint).with(column: 2, row: 3, colour: 'A')
+  describe 'Correct usage' do
+    it 'colours in the specified pixel' do
+      expect(@canvas).to receive(:paint).with(column: 2, row: 3, colour: 'A')
 
-    @colour_pixel.run [2, 3, 'A']
-  end
+      @colour_pixel.run [2, 3, 'A']
+    end
 
-  it 'colours in any pixel' do
-    expect(@canvas).to receive(:paint).with(column: 3, row: 4, colour: 'A')
+    it 'colours in any pixel' do
+      expect(@canvas).to receive(:paint).with(column: 3, row: 4, colour: 'A')
 
-    @colour_pixel.run [3, 4, 'A']
-  end
+      @colour_pixel.run [3, 4, 'A']
+    end
 
-  it 'colours in any pixel with different colours' do
-    expect(@canvas).to receive(:paint).with(column: 4, row: 5, colour: 'B')
+    it 'colours in any pixel with different colours' do
+      expect(@canvas).to receive(:paint).with(column: 4, row: 5, colour: 'B')
 
-    @colour_pixel.run [4, 5, 'B']
-  end
+      @colour_pixel.run [4, 5, 'B']
+    end
 
-  context 'when extra arguments are given' do
-    it 'ignores them' do
-      expect(@canvas).to receive(:paint).with(column: 2, row: 10, colour: 'C')
+    context 'when extra arguments are given' do
+      it 'ignores them' do
+        expect(@canvas).to(
+          receive(:paint).with(column: 2, row: 10, colour: 'C'))
 
-      @colour_pixel.run [2, 10, 'C', 'B']
+        @colour_pixel.run [2, 10, 'C', 'B']
+      end
     end
   end
 end
