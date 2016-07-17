9902b6adc82b2e6c833cf7d6ecd14064dd2f28e2
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-07-17 11:42:27 +0100

    These are examples of correct usage

--- spec/painting_a_horizontal_line_spec.rb
+++ spec/painting_a_horizontal_line_spec.rb
@@ -5,50 +5,52 @@ describe 'Painting a horizonal line on the canvas' do
     @draw_horizontal_line = Commands::PaintHorizontalLine.new(@canvas)
   end
 
-  it 'paints only the horizontal line specified' do
-    (1..5).each do |column|
-      expect(@canvas).to(
-          receive(:paint).with(row: 2, column: column, colour: 'W'))
-    end
-
-    @draw_horizontal_line.run  [1, 5, 2, 'W']
-  end
-
-  it 'paints a horizontal line anywhere' do
-    (1..2).each do |column|
-      expect(@canvas).to(
-          receive(:paint).with(row: 3, column: column, colour: 'W'))
-    end
-
-    @draw_horizontal_line.run [1, 2, 3, 'W']
-  end
-
-  it 'paints a horizontal line anywhere any colour' do
-    (1..2).each do |column|
-      expect(@canvas).to(
-          receive(:paint).with(row: 3, column: column, colour: 'Z'))
-    end
-
-    @draw_horizontal_line.run [1, 2, 3, 'Z']
-  end
-
-  it 'paints a horizontal line from right to left' do
-    (1..2).each do |column|
-      expect(@canvas).to(
-          receive(:paint).with(row: 3, column: column, colour: 'Z'))
-    end
-
-    @draw_horizontal_line.run [2, 1, 3, 'Z']
-  end
-
-  context 'when it receives extra arguments' do
-    it 'ignores them' do
-      (4..5).each do |column|
+  describe 'Correct usage' do
+    it 'paints only the horizontal line specified' do
+      (1..5).each do |column|
         expect(@canvas).to(
-            receive(:paint).with(row: 1, column: column, colour: 'Y'))
+          receive(:paint).with(row: 2, column: column, colour: 'W'))
       end
 
-      @draw_horizontal_line.run [4, 5, 1, 'Y', 'X']
+      @draw_horizontal_line.run  [1, 5, 2, 'W']
+    end
+
+    it 'paints a horizontal line anywhere' do
+      (1..2).each do |column|
+        expect(@canvas).to(
+          receive(:paint).with(row: 3, column: column, colour: 'W'))
+      end
+
+      @draw_horizontal_line.run [1, 2, 3, 'W']
+    end
+
+    it 'paints a horizontal line anywhere any colour' do
+      (1..2).each do |column|
+        expect(@canvas).to(
+          receive(:paint).with(row: 3, column: column, colour: 'Z'))
+      end
+
+      @draw_horizontal_line.run [1, 2, 3, 'Z']
+    end
+
+    it 'paints a horizontal line from right to left' do
+      (1..2).each do |column|
+        expect(@canvas).to(
+          receive(:paint).with(row: 3, column: column, colour: 'Z'))
+      end
+
+      @draw_horizontal_line.run [2, 1, 3, 'Z']
+    end
+
+    context 'when it receives extra arguments' do
+      it 'ignores them' do
+        (4..5).each do |column|
+          expect(@canvas).to(
+            receive(:paint).with(row: 1, column: column, colour: 'Y'))
+          end
+
+        @draw_horizontal_line.run [4, 5, 1, 'Y', 'X']
+      end
     end
   end
 end
