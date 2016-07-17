b1c599a0fa10a572783a7997e9770d9f098345a6
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-07-17 13:12:36 +0100

    Removed unused method

--- lib/colour.rb
+++ lib/colour.rb
@@ -1,9 +1,5 @@
 module Painting
   module Colour
-    def unsupported?(colour)
-      !('A'..'Z').include?(colour)
-    end
-
     def unavailable?(colour)
       !('A'..'Z').include?(colour)
     end
