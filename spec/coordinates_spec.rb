aad3e7ab1befbd48edc46ee2bc6c84a868b0d9f1
Hemal <h.n.varambhia@gmail.com>
2016-07-18 11:06:02 +0100

    Added a creation method to isolate namespaced instanciation in one place

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -4,7 +4,7 @@ describe 'Coordinates' do
   describe '#within_bounds?' do
     context 'when the x-coordinate is below the minimum' do
       it 'confirms the co-ordinate as being out of bounds' do
-        point = Coordinates::Point.new(x: 0, y: 5)
+        point = coordinates(x: 0, y: 5)
         
         expect(point).not_to be_within_bounds
       end
@@ -12,7 +12,7 @@ describe 'Coordinates' do
 
     context 'when the x-coordinates is above the maximum' do
       it 'confirms the co-ordinate as being out of bounds' do
-        point = Coordinates::Point.new(x: 251, y: 5)
+        point = coordinates(x: 251, y: 5)
         
         expect(point).not_to be_within_bounds
       end
@@ -20,7 +20,7 @@ describe 'Coordinates' do
 
     context 'when the y-coordinate is below the minimum' do
       it 'confirms the co-ordinate as being out of bounds' do
-        point = Coordinates::Point.new(x: 10, y: 0)
+        point = coordinates(x: 10, y: 0)
         
         expect(point).not_to be_within_bounds
       end
@@ -28,7 +28,7 @@ describe 'Coordinates' do
 
     context 'when the y-coordinate is above the maximum' do
       it 'confirms the co-ordinate as being out of bounds' do
-        point = Coordinates::Point.new(x: 10, y: 251)
+        point = coordinates(x: 10, y: 251)
         
         expect(point).not_to be_within_bounds
       end
@@ -36,7 +36,7 @@ describe 'Coordinates' do
 
     context 'when the x and y-coordinates are confined in the space' do
       it 'confirms the co-ordinate as being within bounds' do
-        point = Coordinates::Point.new(x: 10, y: 10)
+        point = coordinates(x: 10, y: 10)
         
         expect(point).to be_within_bounds
       end
@@ -46,8 +46,8 @@ describe 'Coordinates' do
   describe '#==' do
     context 'the x co-ordinates are different' do
       it 'confirms them as not being equal' do
-        point_1 = Coordinates::Point.new(x: 1, y: 2)
-        point_2 = Coordinates::Point.new(x: 2, y: 2)
+        point_1 = coordinates(x: 1, y: 2)
+        point_2 = coordinates(x: 2, y: 2)
                                          
         expect(point_1).not_to eq point_2
       end
@@ -55,8 +55,8 @@ describe 'Coordinates' do
 
     context 'the y co-ordinates are different' do
       it 'confirms them as not being equal' do
-        point_1 = Coordinates::Point.new(x: 3, y: 10)
-        point_2 = Coordinates::Point.new(x: 3, y: 1)
+        point_1 = coordinates(x: 3, y: 10)
+        point_2 = coordinates(x: 3, y: 1)
                                          
         expect(point_1).not_to eq point_2
       end
@@ -64,11 +64,15 @@ describe 'Coordinates' do
 
     context 'the both co-ordinates are the same' do
       it 'confirms them as being equal' do
-        point_1 = Coordinates::Point.new(x: 3, y: 4)
-        point_2 = Coordinates::Point.new(x: 3, y: 4)
+        point_1 = coordinates(x: 3, y: 4)
+        point_2 = coordinates(x: 3, y: 4)
                                          
         expect(point_1).to eq point_2
       end
     end
+  end
+
+  def coordinates(coords)
+    Coordinates::Point.new coords
   end
 end
