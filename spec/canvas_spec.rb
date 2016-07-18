866797755f64be940eb1c61ffd16dfe4e4394904
Hemal <h.n.varambhia@gmail.com>
2016-07-18 09:18:29 +0100

    Expectation match the description of the behaviour now

--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -10,13 +10,13 @@ describe 'A Canvas' do
     it 'creates a blank canvas with the specified dimensions' do
       image = @canvas.blank(width: 3 ,height: 3)
 
-      expect(image).to be_width(3).and be_height(3).and be_white
+      expect(@canvas).to be_width(3).and be_height(3).and be_white
     end
 
     it 'creates a blank canvas of any specified dimensions' do
-      image = @canvas.blank(width: 5 ,height: 4)
+      @canvas.blank(width: 5 ,height: 4)
 
-      expect(image).to be_width(5).and be_height(4).and be_white
+      expect(@canvas).to be_width(5).and be_height(4).and be_white
     end
   end
 
@@ -77,7 +77,7 @@ describe 'A Canvas' do
       not_on_canvas = Coordinates::Point.new(x: 3, y: 1)
       colour = @canvas.paint(point: not_on_canvas, colour: 'U')
 
-      expect(@canvas.image).to be_width(2).and be_height(2)
+      expect(@canvas).to be_width(2).and be_height(2)
       expect(colour).to be_nil
     end
 
@@ -85,7 +85,7 @@ describe 'A Canvas' do
       not_on_canvas = Coordinates::Point.new(x: 3, y: 3)
       colour = @canvas.paint(point: not_on_canvas, colour: 'U')
 
-      expect(@canvas.image).to be_width(2).and be_height(2)
+      expect(@canvas).to be_width(2).and be_height(2)
       expect(colour).to be_nil
     end
 
@@ -119,30 +119,33 @@ describe 'A Canvas' do
         point: Coordinates::Point.new(x: 2, y: 2), colour: 'B')
       @canvas.clear
 
-      expect(@canvas.image).to be_width(2).and be_height(2).and be_white
+      expect(@canvas).to be_width(2).and be_height(2).and be_white
     end
 
     it 'clears a canvas with no image' do
       @canvas.clear
 
-      expect(@canvas.image).to be_width(0).and be_height(0)
+      expect(@canvas).to be_width(0).and be_height(0)
     end
   end
 
   RSpec::Matchers.define :be_width do |expected_width|
-    match do |image|
+    match do |canvas|
+      image = canvas.image
       image.all? { |row| row.size == expected_width }
     end
   end
 
   RSpec::Matchers.define :be_height  do |expected_height|
-    match do |image|
+    match do |canvas|
+      image = canvas.image
       image.size == expected_height
     end
   end
 
   RSpec::Matchers.define :be_white do
-    match do |image|
+    match do |canvas|
+      image = canvas.image
       white = Array.new(image.sample.size) {'O'}
       image.all? { |row| row == white }
     end
