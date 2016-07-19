b70c32d64e83e3456a61d23fe430cf5e7e501898
Hemal <h.n.varambhia@gmail.com>
2016-07-19 07:55:32 +0100

    Improved the description of the behaviour being test-driven

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -67,31 +67,33 @@ describe 'Coordinates' do
       expect(point_2).to eq point_3
       expect(point_3).to eq point_1
     end
-    
-    context 'the x co-ordinates are different' do
-      it 'confirms them as not being equal' do
-        point_1 = coordinates(x: 1, y: 2)
-        point_2 = coordinates(x: 2, y: 2)
-                                         
-        expect(point_1).not_to eq point_2
-      end
-    end
 
-    context 'the y co-ordinates are different' do
-      it 'confirms them as not being equal' do
-        point_1 = coordinates(x: 3, y: 10)
-        point_2 = coordinates(x: 3, y: 1)
-                                         
-        expect(point_1).not_to eq point_2
-      end
-    end
+    context 'given two points' do
+      context 'when their x-coordinates are different' do
+        it 'confirms that they are not equal' do
+          point_1 = coordinates(x: 1, y: 2)
+          point_2 = coordinates(x: 2, y: 2)
 
-    context 'the both co-ordinates are the same' do
-      it 'confirms them as being equal' do
-        point_1 = coordinates(x: 3, y: 4)
-        point_2 = coordinates(x: 3, y: 4)
-                                         
-        expect(point_1).to eq point_2
+          expect(point_1).not_to eq point_2
+        end
+      end
+
+      context 'when their y-coordinates are different' do
+        it 'confirms that they are not equal' do
+          point_1 = coordinates(x: 3, y: 10)
+          point_2 = coordinates(x: 3, y: 1)
+
+          expect(point_1).not_to eq point_2
+        end
+      end
+
+      context 'when both their x and y-coordinates are the same' do
+        it 'confirms that they are equal' do
+          point_1 = coordinates(x: 3, y: 4)
+          point_2 = coordinates(x: 3, y: 4)
+
+          expect(point_1).to eq point_2
+        end
       end
     end
   end
