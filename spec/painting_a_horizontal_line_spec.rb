2ec30777fd96823c2cd44c3040acc86ee94f703b
Hemal <h.n.varambhia@gmail.com>
2016-07-19 13:26:49 +0100

    Code is more clear about the line that is expected to be drawn and how that relates to the args used

--- spec/painting_a_horizontal_line_spec.rb
+++ spec/painting_a_horizontal_line_spec.rb
@@ -12,8 +12,8 @@ describe 'Painting a horizonal line on the canvas' do
 
   describe 'Correct usage' do
     it 'paints only the horizontal line specified' do
-      (1..5).each do |x_coord|
-        point = coordinates(x: x_coord, y: 2)
+      from, to = coordinates(x: 1, y: 2), coordinates(x: 5, y: 2)
+      horizontal_line(from, to).each do |point|
         expect(@canvas).to receive(:paint).with(point: point, colour: 'W')
       end
 
@@ -21,8 +21,8 @@ describe 'Painting a horizonal line on the canvas' do
     end
 
     it 'paints a horizontal line anywhere' do
-      (1..2).each do |x_coord|
-        point = coordinates(x: x_coord, y: 3)
+      from, to = coordinates(x: 1, y: 3), coordinates(x: 2, y: 3)
+      horizontal_line(from, to).each do |point|
         expect(@canvas).to receive(:paint).with(point: point, colour: 'W')
       end
 
@@ -30,8 +30,8 @@ describe 'Painting a horizonal line on the canvas' do
     end
 
     it 'paints a horizontal line anywhere any colour' do
-      (1..2).each do |x_coord|
-        point = coordinates(x: x_coord, y: 3)
+      from, to = coordinates(x: 1, y: 3), coordinates(x: 2, y: 3)
+      horizontal_line(from, to).each do |point|
         expect(@canvas).to receive(:paint).with(point: point, colour: 'Z')
       end
 
@@ -39,8 +39,8 @@ describe 'Painting a horizonal line on the canvas' do
     end
 
     it 'paints a horizontal line from right to left' do
-      (1..2).each do |x_coord|
-        point = coordinates(x: x_coord, y: 3)
+      from, to = coordinates(x: 2, y: 3), coordinates(x: 1, y: 3)
+      horizontal_line(from, to).each do |point|
         expect(@canvas).to receive(:paint).with(point: point, colour: 'Z')
       end
 
@@ -49,13 +49,20 @@ describe 'Painting a horizonal line on the canvas' do
 
     context 'when it receives extra arguments' do
       it 'ignores them' do
-        (4..5).each do |x_coord|
-          point = coordinates(x: x_coord, y: 1)
+        from, to = coordinates(x: 4, y: 1), coordinates(x: 5, y: 1)
+        horizontal_line(from, to).each do |point|
           expect(@canvas).to receive(:paint).with(point: point, colour: 'Y')
         end
 
         @draw_horizontal_line.run [4, 5, 1, 'Y', 'X']
       end
+    end
+
+    def horizontal_line(from, to)
+      start = [from.x, to.x].min
+      finish = [from.x, to.x].max
+
+      (start..finish).map { |x_coord| coordinates(x: x_coord, y: from.y) }
     end
   end
 
