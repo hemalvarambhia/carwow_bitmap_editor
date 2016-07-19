c325103f2a95c6d009a075e552ef050019d8b3dc
Hemal <h.n.varambhia@gmail.com>
2016-07-19 13:33:56 +0100

    Code is more clear about the line that is expected to be drawn and how that relates to the args used

--- spec/painting_a_vertical_line_spec.rb
+++ spec/painting_a_vertical_line_spec.rb
@@ -12,8 +12,8 @@ describe 'Painting a vertical line on the canvas' do
 
   describe 'Correct usage' do
     it 'paints a vertical line on the specified part' do
-      (3..6).each do |y_coord|
-        point = coordinates(x: 2, y: y_coord)
+      from, to = coordinates(x: 2, y: 3), coordinates(x: 2, y: 6)
+      vertical_line(from, to).each do |point|
         expect(@canvas).to receive(:paint).with(point: point, colour: 'W')
       end
 
@@ -21,8 +21,8 @@ describe 'Painting a vertical line on the canvas' do
     end
 
     it 'paints a vertical line anywhere' do
-      (1..3).each do |y_coord|
-        point = coordinates(x: 5, y: y_coord)
+      from, to = coordinates(x: 5, y: 1), coordinates(x: 5, y: 3)
+      vertical_line(from, to).each do |point|
         expect(@canvas).to receive(:paint).with(point: point, colour: 'W')
       end
 
@@ -30,8 +30,8 @@ describe 'Painting a vertical line on the canvas' do
     end
 
     it 'paints a vertical line anywhere in any colour' do
-      (2..4).each do |y_coord|
-        point = coordinates(x: 4, y: y_coord)
+      from, to = coordinates(x: 4, y: 2), coordinates(x: 4, y: 4)
+      vertical_line(from, to).each do |point|
         expect(@canvas).to receive(:paint).with(point: point, colour: 'H')
       end
 
@@ -39,8 +39,8 @@ describe 'Painting a vertical line on the canvas' do
     end
 
     it 'paints a vertical line in an upward direction' do
-      (2..4).each do |y_coord|
-        point = coordinates(x: 4, y: y_coord)
+      from, to = coordinates(x: 4, y: 4), coordinates(x: 4, y: 2)
+      vertical_line(from, to).each do |point|
         expect(@canvas).to receive(:paint).with(point: point, colour: 'H')
       end
 
@@ -49,13 +49,20 @@ describe 'Painting a vertical line on the canvas' do
 
     context 'when it receives extra parameters' do
       it 'ignores them' do
-        (2..3).each do |y_coord|
-          point = coordinates(x: 1, y: y_coord)
+        from, to = coordinates(x: 1, y: 2), coordinates(x: 1, y: 3)
+        vertical_line(from, to).each do |point|
           expect(@canvas).to receive(:paint).with(point: point, colour: 'H')
         end
 
         @draw_vertical_line.run [1, 2, 3, 'H', 'N']
       end
+    end
+
+    def vertical_line(from, to)
+      start = [from.y, to.y].min
+      finish = [from.y, to.y].max
+
+      (start..finish).map {|y_coord| coordinates(x: from.x, y: y_coord) }
     end
   end
 
